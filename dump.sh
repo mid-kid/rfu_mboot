99edb119e552b23b5ab4a9339b71a2a08f592449
mid-kid <esteve.varela@gmail.com>
2023-06-02 02:26:23 +0200

    Dump strings

--- dump.sh
+++ dump.sh
@@ -2,11 +2,16 @@
 set -e
 dd if=baserom.bin bs=1 skip=672 2>/dev/null | ./dump_lz77.py > baserom_demo.bin
 
-off=416
+off=256
 split() {
-    dd if=baserom_demo.bin bs=1 skip=$off count=$2 2>/dev/null | \
+    skip=$(expr $off - 256 || true)
+    size=$(expr $(printf %d $2) - $off)
+    dd if=baserom_demo.bin bs=1 skip=$skip count=$size 2>/dev/null | \
         ./dump_bin2c.py $1 > baserom_demo_$(printf "%04x" $off).dump.c
-    off=$(expr $off + $2)
+    off=$(expr $off + $size)
 }
 
-split .text 21476
+split .text 0x2a0
+split .text 0x3fb8
+split .rodata 0x4200
+split .rodata 0x5684
