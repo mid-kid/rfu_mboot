ad6bc85c7461b7014410d7e2a7fadbab6adcf6f3
mid-kid <esteve.varela@gmail.com>
2023-06-03 14:34:44 +0200

    Dump code with the appropriate metadata

--- dump_bin2c.py
+++ dump_bin2c.py
@@ -2,11 +2,24 @@
 
 from sys import argv, stdin, stdout
 
-stdout.write("__attribute__((section(\"%s\"))) " % argv[1])
-stdout.write("static const unsigned char data[]={")
-while True:
+stdout.write("__asm__(\"\n")
+stdout.write(".section %s\n" % argv[1])
+if argv[1] == ".text":
+    stdout.write(".type data, function\n")
+else:
+    stdout.write(".type data, object\n")
+stdout.write(".thumb_func\n")
+stdout.write("data:\n")
+
+stdout.write(".byte ")
+byte = stdin.buffer.read(1)
+while byte:
+    stdout.write("0x%02x" % byte[0])
     byte = stdin.buffer.read(1)
     if not byte:
         break
-    stdout.write("0x%02x," % byte[0])
-stdout.write("};")
+    stdout.write(",")
+stdout.write("\n")
+
+stdout.write(".size data, .-data\n")
+stdout.write("\");")
