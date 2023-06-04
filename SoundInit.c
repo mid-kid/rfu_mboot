b7892443e692b40a0e894c20d5967fe73d1c2fbd
mid-kid <esteve.varela@gmail.com>
2023-06-05 01:42:08 +0200

    Decompile SoundInit and SoundStop

--- SoundInit.c
+++ SoundInit.c
@@ -1,12 +1,14 @@
-#if 1
-__asm__("
-.section .text
-@.global SoundInit
-.type SoundInit, function
-.thumb_func
-SoundInit:
-.2byte 0xb500,0x4909,0x2080,0x8008,0x3904,0x4a08,0x1c10,0x8008,0x3102,0x2002,0x8008,0x3106,0x22c2,0x0212,0x1c10,0x8008,0xf000,0xf862,0xbc01,0x4700,0x0084,0x0400,0xff77,0x0000
-.size SoundInit, .-SoundInit
-");
-#else
-#endif
+#include <Agb.h>
+
+extern void SoundStop(void);
+
+void SoundInit(void)
+{
+    *(vu16 *)REG_SOUNDCNT_X = SOUND_DMG_ON;
+    *(vu16 *)REG_SOUNDCNT_L = SOUND_DMG_ALL_SO_ON << 8 |
+        7 << SOUND_SO1_LEVEL_SHIFT |
+        7 << SOUND_SO2_LEVEL_SHIFT;
+    *(vu16 *)REG_SOUNDCNT_H = SOUND_DMG_MIX_FULL;
+    *(vu16 *)REG_SOUNDBIAS = 3 << 14 | 0x200;
+    SoundStop();
+}
--- SoundStop.c
+++ SoundStop.c
@@ -1,12 +1,14 @@
-#if 1
-__asm__("
-.section .text
-@.global SoundStop
-.type SoundStop, function
-.thumb_func
-SoundStop:
-.2byte 0x4802,0x2100,0x7241,0x21ff,0x7201,0x4770,0x5780,0x0300
-.size SoundStop, .-SoundStop
-");
-#else
-#endif
+#include <Agb.h>
+
+extern struct Sound {
+    u16 *basePtr;
+    u16 *playPtr;
+    u8 time;
+    u8 sfxNum;
+} Sound;
+
+void SoundStop(void)
+{
+    Sound.sfxNum = 0;
+    Sound.time = -1;
+}
