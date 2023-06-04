3468367c6e0fb73ff1314d4122a52d2f8b535de3
mid-kid <esteve.varela@gmail.com>
2023-06-05 01:00:14 +0200

    Decompile BgScSet

---
+++ BgScSet.c
@@ -0,0 +1,14 @@
+#include <Agb.h>
+
+extern u16 BgBak[32*20];
+
+u16 *BgScSet(u16 Pos, u16 PlttNo, char *Srcp)
+{
+    u16 *bg;
+
+    bg = BgBak + Pos;
+    while (*Srcp != 0) {
+        *bg++ = *Srcp++ | PlttNo << 0xc;
+    }
+    return bg;
+}
--- dump.sh
+++ dump.sh
@@ -37,7 +37,7 @@ split .text 0x148c BgScClearGame
 split .text 0x14f0 VramDrawBg2_MainMenu
 split .text 0x1528 VramDrawBg2_SearchMenu
 split .text 0x1548 VramClearBg2
-split .text 0x157c
+split .text 0x157c BgScSet
 split .text 0x15c4 BgScClear
 split .text 0x15dc Delay3s
 split .text 0x1614 WinInit
