3fbd74df4fd547e6164be0df4c3830bad44d9030
mid-kid <esteve.varela@gmail.com>
2023-06-05 16:15:18 +0200

    Decompile FrameCountReset

--- FrameCountReset.c
+++ FrameCountReset.c
@@ -1,12 +1,8 @@
-#if 1
-__asm__("
-.section .text
-@.global FrameCountReset
-.type FrameCountReset, function
-.thumb_func
-FrameCountReset:
-.2byte 0x4901,0x2000,0x7008,0x4770,0x5698,0x0300
-.size FrameCountReset, .-FrameCountReset
-");
-#else
-#endif
+#include <Agb.h>
+
+extern u8 FrameCount;
+
+void FrameCountReset(void)
+{
+    FrameCount = 0;
+}
