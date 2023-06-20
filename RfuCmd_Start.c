cc1d4915ce35c17011d187dcae5045216d337daf
mid-kid <esteve.varela@gmail.com>
2023-06-20 23:19:01 +0200

    Decompile RfuCmd_Start

--- RfuCmd_Start.c
+++ RfuCmd_Start.c
@@ -1,12 +1,21 @@
-#if 1
-__asm__("
-.section .text
-@.global RfuCmd_Start
-.type RfuCmd_Start, function
-.thumb_func
-RfuCmd_Start:
-.2byte 0xb500,0xf7ff,0xff09,0x4909,0x4809,0x6008,0x4909,0x2000,0x7208,0xf7ff,0xff3d,0x0400,0x0c00,0x2801,0xd00e,0x4806,0x2100,0xf7ff,0xff99,0x0400,0x0c00,0xe008,0x5cc0,0x0300,0x0010,0x9966,0x5ca0,0x0300,0x0090,0x9966,0x2005,0xbc02,0x4708,0x0000
-.size RfuCmd_Start, .-RfuCmd_Start
-");
-#else
-#endif
+#include <Agb.h>
+
+#include "Rfu.h"
+extern u32 RfuCmdInit(void);
+extern u16 RfuCmdSend(void);
+extern u16 RfuCmdRecv(u32 Cmd, u8 VarSize);
+extern u32 RfuBufSend[0x48];
+extern struct Rfu Rfu;
+
+u16 RfuCmd_Start(void)
+{
+    RfuCmdInit();
+    RfuBufSend[0] = 0x99660010;
+    Rfu.field2_0x8 = 0;
+
+    if (RfuCmdSend() == 1) {
+        return 5;
+    } else {
+        return RfuCmdRecv(0x99660090, FALSE);
+    }
+}
