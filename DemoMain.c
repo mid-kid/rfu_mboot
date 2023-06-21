3ce8b4ab537007842f58d2d7b9a077df31b42393
mid-kid <esteve.varela@gmail.com>
2023-06-22 01:54:31 +0200

    Decompile DemoMain

--- DemoMain.c
+++ DemoMain.c
@@ -1,12 +1,69 @@
-#if 1
-__asm__("
-.section .text
-.global DemoMain
-.type DemoMain, function
-.thumb_func
-DemoMain:
-.2byte 0xb530,0xb081,0x4d2f,0x2400,0x802c,0x492f,0x2014,0x8008,0x201d,0xf003,0xfe11,0x2000,0x9000,0x4a2c,0x4668,0x6010,0x492b,0x6051,0x482b,0x1a40,0x0880,0x2185,0x0609,0x4308,0x6090,0x6890,0x4928,0x4829,0x6008,0x4829,0x4929,0xf003,0xfdf9,0x4829,0x21a0,0x04c9,0xf003,0xfdf4,0x4827,0x4928,0xf003,0xfdf0,0xf001,0xf9bc,0xf001,0xf970,0x4925,0x2004,0x8008,0x3102,0x4a24,0x1c10,0x8008,0x3102,0x4a23,0x1c10,0x8008,0x3102,0x4a22,0x1c10,0x8008,0x390e,0x2290,0x0112,0x1c10,0x8008,0xf002,0xfab6,0x491e,0x2081,0x8008,0x491e,0x2008,0x8008,0x2001,0x8028,0x2008,0xf002,0xfcfb,0x481b,0x7004,0x481b,0x7004,0xf000,0xf839,0x491a,0x481a,0x6008,0x1c0c,0xf003,0xfdcf,0xf001,0xf84b,0x6820,0xf003,0xfdce,0xe7f7,0x0000,0x0208,0x0400,0x0204,0x0400,0x00d4,0x0400,0x5684,0x0300,0x6310,0x0300,0x7ffc,0x0300,0x01fc,0x0300,0x460c,0x0300,0x4000,0x0600,0x43c8,0x0300,0x4420,0x0300,0x1800,0x0600,0x0008,0x0400,0x0105,0x0000,0x0206,0x0000,0x0307,0x0000,0x0200,0x0400,0x0004,0x0400,0x5764,0x0300,0x5758,0x0300,0x5c90,0x0300,0x04c5,0x0300
-.size DemoMain, .-DemoMain
-");
-#else
-#endif
+#include <Agb.h>
+
+extern void intr_main(void);
+extern void SoundInit(void);
+extern void WinInit(void);
+extern void Init_Unk2894(void);
+extern void Init_Unk2d34(u8 param_1);
+extern void MainMenuInit(void);
+extern void MainMenu(void);
+extern void KeyRead(void);
+extern u8 LZ_460c[];
+extern u8 LZ_43c8[];
+extern u8 LZ_4420[];
+extern u8 MainMenuFadeOut;
+extern u8 Lang;
+extern void (*Proc)();
+extern u8 bss_start[];
+extern u8 bss_end[];
+
+void DemoMain(void)
+{
+    *(vu16 *)REG_IME = 0;
+    *(vu16 *)REG_WAITCNT = CST_ROM0_2ND_1WAIT | CST_ROM0_1ST_3WAIT;
+    RegisterRamReset(RESET_ALL_RAM_FLAG & ~RESET_CPU_WRAM_FLAG);
+    DmaClear(3, 0, bss_start, (u32)(bss_end - bss_start), 32);
+    IntrAddrSet(intr_main);
+
+    LZ77UnCompVram(LZ_460c, (void *)0x6004000);
+    LZ77UnCompVram(LZ_43c8, (void *)0x5000000);
+    LZ77UnCompVram(LZ_4420, (void *)0x6001800);
+    SoundInit();
+    WinInit();
+
+    *(vu16 *)REG_BG0CNT = 
+        0 << BG_SCREEN_BASE_SHIFT |
+        1 << BG_CHAR_BASE_SHIFT |
+        0 << BG_PRIORITY_SHIFT;
+    *(vu16 *)REG_BG1CNT =
+        1 << BG_SCREEN_BASE_SHIFT | 
+        1 << BG_CHAR_BASE_SHIFT |
+        1 << BG_PRIORITY_SHIFT;
+    *(vu16 *)REG_BG2CNT =
+        2 << BG_SCREEN_BASE_SHIFT |
+        1 << BG_CHAR_BASE_SHIFT |
+        2 << BG_PRIORITY_SHIFT;
+    *(vu16 *)REG_BG3CNT =
+        3 << BG_SCREEN_BASE_SHIFT |
+        1 << BG_CHAR_BASE_SHIFT |
+        3 << BG_PRIORITY_SHIFT;
+    *(vu16 *)REG_DISPCNT = DISP_BG0_ON | DISP_BG3_ON;
+
+    Init_Unk2894();
+    *(vu16 *)REG_IE = SIO_INTR_FLAG | V_BLANK_INTR_FLAG;
+    *(vu16 *)REG_STAT = STAT_V_BLANK_IF_ENABLE;
+    *(vu16 *)REG_IME = 1;
+
+    Init_Unk2d34(8);
+    MainMenuFadeOut = FALSE;
+    Lang = 0;
+
+    MainMenuInit();
+    Proc = MainMenu;
+
+    for (;;) {
+        VBlankIntrWait();
+        KeyRead();
+        Proc();
+    }
+}
--- defs.s
+++ defs.s
@@ -6,3 +6,5 @@
 tdef FUN_03003078, 0x03003078
 tdef FUN_03003e04, 0x03003e04
 tdef Sio32IntrProc_Unk0e10, 0x03000e10
+tdef Init_Unk2894, 0x03002894
+tdef Init_Unk2d34, 0x03002d34
