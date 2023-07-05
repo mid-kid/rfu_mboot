7dc4cc159c24a20d7f0612544c5c450653221ebb
mid-kid <esteve.varela@gmail.com>
2023-07-05 16:08:52 +0200

    Make progress.sh more exciting

--- progress.sh
+++ progress.sh
@@ -4,4 +4,4 @@ baserom="$(grep 'dump/[^/]*\.o(\.text)' demo.ld | wc -l)"
 done="$(expr $total - $baserom)"
 
 percent="$(expr $(expr $done \* 10000) / $total)"
-printf '%d.%02d%%\n' "$(expr $percent / 100)" "$(expr $percent % 100)"
+printf '%d.%02d%% (%d/%d)\n' "$(expr $percent / 100)" "$(expr $percent % 100)" "$done" "$total"
