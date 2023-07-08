57ce52fd8770d7b423b69642b243a9ebdc76b126
mid-kid <esteve.varela@gmail.com>
2023-07-09 00:33:16 +0200

    Initialize work for rodata dumping

--- progress.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-total="$(grep '\.o(\.text)' demo.ld | wc -l)"
-baserom="$(grep 'dump/[^/]*\.o(\.text)' demo.ld | wc -l)"
-done="$(expr $total - $baserom)"
-
-percent="$(expr $(expr $done \* 10000) / $total)"
-printf '%d.%02d%% (%d/%d)\n' "$(expr $percent / 100)" "$(expr $percent % 100)" "$done" "$total"
