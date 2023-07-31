f745aa5607cd24fff5e8ecd91d790539f10f5b92
mid-kid <esteve.varela@gmail.com>
2023-07-31 17:10:38 +0200

    Merge Sound library

--- Sound.c
+++ Sound.c
@@ -6,6 +6,20 @@ extern struct Sound {
     u8 time;
     u8 sfxNum;
 } Sound;
+extern u16 *SoundSfxTable[];
+void SoundStop(void);
+
+void SoundInit(void)
+{
+    *(vu16 *)REG_SOUNDCNT_X = SOUND_DMG_ON;
+    *(vu16 *)REG_SOUNDCNT_L =
+        SOUND_DMG_ALL_SO_ON << 8 |
+        7 << SOUND_SO1_LEVEL_SHIFT |
+        7 << SOUND_SO2_LEVEL_SHIFT;
+    *(vu16 *)REG_SOUNDCNT_H = SOUND_DMG_MIX_FULL;
+    *(vu16 *)REG_SOUNDBIAS = 3 << 14 | 0x200;
+    SoundStop();
+}
 
 void SoundMain(void)
 {
@@ -38,7 +52,22 @@ void SoundMain(void)
             *soundCnt = *Sound.playPtr++;
             break;
         }
-
     }
-    Sound.time = Sound.time - 1;
+
+    Sound.time--;
+}
+
+void SoundPlaySfx(u8 Num)
+{
+    if (Sound.sfxNum > Num && Sound.time != (u8)-1) return;
+    Sound.playPtr = SoundSfxTable[Num];
+    Sound.basePtr = Sound.playPtr;
+    Sound.time = 0;
+    Sound.sfxNum = Num;
+}
+
+void SoundStop(void)
+{
+    Sound.sfxNum = 0;
+    Sound.time = -1;
 }
