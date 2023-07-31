4de3d0c7dcaafdccdf1bea5463b0c928fdd449c4
mid-kid <esteve.varela@gmail.com>
2023-07-31 17:40:46 +0200

    Move VBlankIntr into interrupt.c

--- interrupt.c
+++ interrupt.c
@@ -2,8 +2,21 @@
 
 extern void Sio32Intr(void);
 extern void VBlankIntr(void);
+extern void SoundMain(void);
+extern void rfu_syncVBlank(void);
+extern u16 Bg0Bak[32*20];
 
 void (*const IntrTable[])(void) = {
     Sio32Intr,
     VBlankIntr
 };
+
+void VBlankIntr(void)
+{
+    DmaArrayCopy(3, Bg0Bak, VRAM, 32);
+
+    SoundMain();
+    rfu_syncVBlank();
+
+    *(vu16 *)INTR_CHECK_BUF = 1;
+}
